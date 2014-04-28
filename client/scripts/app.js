bd3ef702f6e9ff798ab86c79b5918bcac321fe8e
RP-3 <sarith21@gmail.com>
2014-04-28 14:52:33 -0700

    Created fetch and post

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,39 @@
 // YOUR CODE HERE:
 
+var app = {};
+
+app.init = function(){};
+
+app.send = function(message){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.log(data)
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function(){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      console.log(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
