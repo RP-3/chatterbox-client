042d8a86bfed927373a339f4b98dbd8bc6960dad
RP-3 <sarith21@gmail.com>
2014-04-28 17:01:08 -0700

    Completed view logic for chat messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,8 +1,13 @@
 // YOUR CODE HERE:
+$(document).ready(function(){
+
+})
 
 var app = {};
 
-app.init = function(){};
+app.init = function(){
+  app.fetch();
+};
 
 app.send = function(message){
   $.ajax({
@@ -16,7 +21,6 @@ app.send = function(message){
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.log(data)
       console.error('chatterbox: Failed to send message');
     }
   });
@@ -27,13 +31,58 @@ app.fetch = function(){
     // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
+    data: {order: '-createdAt', limit: 100},
     success: function (data) {
-      console.log('chatterbox: Message sent');
+      console.log('chatterbox: Message fetched');
       console.log(data);
+      app.handleFetch(data);
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.error('chatterbox: Failed to send message');
+      console.error('chatterbox: Failed to receive message');
     }
   });
 };
+
+app.clearMessages = function(){
+  $('#chats').html('');
+};
+
+app.addMessage = function(message){
+  //console.log(message);
+  var appendix = $('<div/>')
+    .append($('<a class="username" href="#" />')
+      .append($('<span/>').text(message.username))
+    )
+    .append($('<span/>').text(" : " + message.text))
+    .append($('<span/>').text(" @ " + message.roomname))
+  // var appendix = $('<div>').html(
+  //   '<a class="username" href="#">'+ cleanedUsername +'</a>: ' + cleanedText + '@ ' + cleanedRoomname
+
+  $('#chats').append(appendix);
+};
+
+app.addRoom = function(roomName){
+  var button = $('<button>').text(roomName);
+  var room = $('<li>').append(button);
+  $('#roomSelect').append(room);
+};
+
+app.addFriend = function(friend){
+
+};
+
+app.handleFetch = function(data){
+  app.messages = data.results;
+    _.each(this.messages, function(value){
+      app.addMessage(value);
+    });
+
+  app.addMessage(data);
+  app.roomnames = _.unique(_.pluck(app.messages, 'roomname'));
+  for (var i = 0; i < app.roomnames.length; i++) {
+    app.addRoom(app.roomnames[i]);
+  }
+};
+
+app.init();
